0ca75d18912b598bb2fcd82a233907abcdbc07dc
SwatiPareek <38918266+SwatiPareek@users.noreply.github.com>
2020-05-30 17:45:47 -0400

    Changed name for unit test -Addressing PR comments

--- tests/test_exponents_and_logs.py
+++ tests/test_exponents_and_logs.py
@@ -2,7 +2,7 @@ import unittest
 import functions.exponents_and_logs as exp
 
 
-class EulerTest(unittest.TestCase):
+class ExponentsTest(unittest.TestCase):
     def test_e(self):
         self.assertAlmostEqual(exp.generate_e(), 2.718281828, 9)
 
@@ -16,6 +16,7 @@ class EulerTest(unittest.TestCase):
     def test_generate_e_input_of_rational(self):
         self.assertAlmostEqual(exp.generate_e(0.5), 1.648721271)
         self.assertAlmostEqual(exp.generate_e(5 / 8), 1.868245957)
+
 
 if __name__ == '__main__':
     unittest.main()
