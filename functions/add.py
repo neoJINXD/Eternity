79c9b859e40ba500faeb941ad47a3148fa2fcc79
neoJINXD <anik_patel@live.com>
2020-05-30 02:25:29 -0400

    Added comments and example of raising exception

--- functions/add.py
+++ functions/add.py
@@ -1,2 +1,4 @@
-def add(x,y):
-    return x+y
+def add(x, y):
+    if y % 2 == 0:
+        raise ValueError
+    return x + y
--- functions/test_add.py
+++ functions/test_add.py
@@ -1,10 +1,18 @@
 import unittest
 from add import add
 
-class TempTest(unittest.TestCase):
+
+# test class
+class TestAdd(unittest.TestCase):
+    # make sure to write good tests (all the edge cases), not just a lot of cases
     def test_add(self):
-        self.assertEquals(add(1,2), 3)
+        self.assertEquals(add(1, 1), 2)
+    
+    def test_raise_error(self):
+        with self.assertRaises(ValueError):
+            add(1, 2)
 
 
+# Allows us to run the tests immediately when this file is run
 if __name__ == '__main__':
     unittest.main()
