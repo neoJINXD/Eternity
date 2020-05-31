67823e687c8f9d839982589c77ec7fb600f1e2b8
neoJINXD <anik_patel@live.com>
2020-05-31 17:10:34 -0400

    Added more tests for sin function

--- tests/test_sin.py
+++ tests/test_sin.py
@@ -1,23 +1,37 @@
 import unittest
 from functions.sin import sin
-from math import pi
+
+# temporary const for PI
+PI: float = 3.1415926535897932384626433832795
 
 
 class TestSin(unittest.TestCase):
 
-    def test_sin_(self):
+    def test_sin_random_number(self):
         self.assertAlmostEqual(sin(6), -0.2794154982, 9)
 
     def test_sin_pi_multiples(self):
         self.assertAlmostEqual(sin(0), 0, 9)
-        self.assertAlmostEqual(sin(pi), 0, 9)
-        self.assertAlmostEqual(sin(2*pi), 0, 9)
-        self.assertAlmostEqual(sin(-1*pi), 0, 9)
+        self.assertAlmostEqual(sin(PI), 0, 9)
+        self.assertAlmostEqual(sin(2*PI), 0, 9)
+        self.assertAlmostEqual(sin(-1*PI), 0, 9)
 
     def test_trig_unit_circle(self):
-        pass
-        # self.assertAlmostEqual(sin(), , 9)
-
+        self.assertAlmostEqual(sin(PI/6), 0.5, 9)
+        self.assertAlmostEqual(sin(PI/4), 0.707106781186, 9)
+        self.assertAlmostEqual(sin(PI/3), 0.866025403784, 9)
+        self.assertAlmostEqual(sin(PI/2), 1, 9)
+        self.assertAlmostEqual(sin(2*PI/3), 0.866025403784, 9)
+        self.assertAlmostEqual(sin(3*PI/4), 0.707106781186, 9)
+        self.assertAlmostEqual(sin(5*PI/6), 0.5, 9)
+        self.assertAlmostEqual(sin(-PI/6), -0.5, 9)
+        self.assertAlmostEqual(sin(-PI/4), -0.707106781186, 9)
+        self.assertAlmostEqual(sin(-PI/3), -0.866025403784, 9)
+        self.assertAlmostEqual(sin(-PI/2), -1, 9)
+        self.assertAlmostEqual(sin(-2*PI/3), -0.866025403784, 9)
+        self.assertAlmostEqual(sin(-3*PI/4), -0.707106781186, 9)
+        self.assertAlmostEqual(sin(-5*PI/6), -0.5, 9)
+        
 
 if __name__ == '__main__':
     unittest.main()
