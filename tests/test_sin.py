c4b9eb8a856c75b2b6fd6f8339e0b67c6d8596eb
neoJINXD <anik_patel@live.com>
2020-05-31 17:16:45 -0400

    More generic sin test cases

--- tests/test_sin.py
+++ tests/test_sin.py
@@ -6,11 +6,18 @@ PI: float = 3.1415926535897932384626433832795
 
 
 class TestSin(unittest.TestCase):
+    '''
+    Unit tests for most common cases where sin is used
+    '''
 
     def test_sin_random_number(self):
         self.assertAlmostEqual(sin(6), -0.2794154982, 9)
+        self.assertAlmostEqual(sin(1.75), 0.9839859468, 9)
+        self.assertAlmostEqual(sin(3.6), -0.4425204432, 9)
+        self.assertAlmostEqual(sin(-0.7), -0.6442176872, 9)
 
     def test_sin_pi_multiples(self):
+        self.assertAlmostEqual(sin(3.1415926535), 0, 9)
         self.assertAlmostEqual(sin(0), 0, 9)
         self.assertAlmostEqual(sin(PI), 0, 9)
         self.assertAlmostEqual(sin(2*PI), 0, 9)
