e9d18af887834f8ec9b991ee669969122135b6a3
neoJINXD <anik_patel@live.com>
2020-06-01 01:51:43 -0400

    Optimized taylor expansion based on Emanuel's suggestion

--- functions/sin.py
+++ functions/sin.py
@@ -1,14 +1,24 @@
-from math import factorial
-
-
 def sin(x: float) -> float:
-    result = 0
-    sign = 1
+    '''
+    Optimized version of sin using taylor expansion.
+    Based on Emanuel's suggestion to avoid factorial
+    and exponent calls.
 
-    for i in range(50):
-        result += ((x**((i*2) + 1)) / factorial(((i*2) + 1))) * sign
+    Each term is it's previous value multiplied by
+    -(x*x)/(n(n-1)), where the starting point is x
+    '''
+    # Initial values
+    result = x  # accumulator for the result
+    older = x   # keeps track of the previous term
+    sign = -1   # the sign needs to flip for every term
+
+    for i in range(1, 50):
+        n = (i * 2) + 1
+        num = x * x
+        denom = n * (n - 1)
+        latest = older * (num / denom)
+        result += sign * latest
+        older = latest
         sign *= -1
 
     return result
-    # math based on
-    # https://en.wikibooks.org/wiki/Trigonometry/Power_Series_for_Cosine_and_Sine
