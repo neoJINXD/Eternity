4ac1403137a073f38db3f8c544f29522af318ddf
cpotteck <cpotteck@gmail.com>
2020-05-31 22:44:14 -0400

    10^x and pi^x implementations using the generic calculate_exponent for calculations

--- tests/test_exponents_and_logs.py
+++ tests/test_exponents_and_logs.py
@@ -1,9 +1,13 @@
 import unittest
-import functions.exponents_and_logs as exp
 import exceptions.exceptions as the_exception
+import functions.exponents_and_logs as exp
+import functions.trignometry as trig
 
 
 class ExponentsTest(unittest.TestCase):
+
+    PI = trig.generate_pi()
+
     def test_e(self):
         self.assertAlmostEqual(exp.generate_e(), 2.718281828, 9)
 
@@ -30,6 +34,24 @@ class ExponentsTest(unittest.TestCase):
         self.assertAlmostEqual(exp.calculate_exponent(3, (1 / 3)), 3 ** (1 / 3), delta=0.02)
         self.assertAlmostEqual(exp.calculate_exponent(100, (1 / 20)), 100 ** (1 / 20), delta=0.03)
         self.assertAlmostEqual(exp.calculate_exponent(200, (1 / 40)), 200 ** (1 / 40), delta=0.03)
+
+    def test_power_of_ten(self):
+        self.assertAlmostEqual(exp.power_of_ten(0), 1)
+        self.assertAlmostEqual(exp.power_of_ten(1), 10)
+        self.assertAlmostEqual(exp.power_of_ten(2), 100)
+        self.assertAlmostEqual(exp.power_of_ten(-4), 10**(-4))
+        self.assertAlmostEqual(exp.power_of_ten(0.01), 10**(0.01))
+        self.assertAlmostEqual(exp.power_of_ten(self.PI), 10**self.PI)
+        self.assertRaises(the_exception.CalculationError, exp.power_of_ten, 'invalid')
+
+    def test_power_of_pi(self):
+        self.assertAlmostEqual(exp.power_of_pi(0), 1)
+        self.assertAlmostEqual(exp.power_of_pi(1), self.PI)
+        self.assertAlmostEqual(exp.power_of_pi(2), self.PI**2)
+        self.assertAlmostEqual(exp.power_of_pi(-4), self.PI**(-4)) # TODO still innacurate
+        self.assertAlmostEqual(exp.power_of_pi(0.01), self.PI**0.01)
+        self.assertAlmostEqual(exp.power_of_pi(self.PI), self.PI**self.PI) # TODO still innacurate
+        self.assertRaises(the_exception.CalculationError, exp.power_of_pi, True)
 
     #I will need to fix this test
     def test_exceptions(self):
