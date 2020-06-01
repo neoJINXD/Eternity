99f2e8bbd4802c56c58d5cb21b3924f932951aa3
nguyen <nguyennguyen8251@gmail.com>
2020-05-31 22:00:40 -0400

    minor fix

--- app/__init__.py
+++ app/__init__.py
@@ -50,6 +50,7 @@ def calcEval(equation):
   try:
     equation = re.sub('(?<=\d|\))(\()', '*(', equation)
     equation = equation.replace('π','PI')
+	equation = equation.replace('e','E')
     result = nsp.eval(equation)
     return result
   except:
