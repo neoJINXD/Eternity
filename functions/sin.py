52ed07b7cfbe44f7e96e18a0cf81616f6ed83289
neoJINXD <anik_patel@live.com>
2020-06-01 13:44:09 -0400

    More optimization for the math used

--- functions/sin.py
+++ functions/sin.py
@@ -9,16 +9,14 @@ def sin(x: float) -> float:
     '''
     # Initial values
     result = x  # accumulator for the result
-    older = x   # keeps track of the previous term
-    sign = -1   # the sign needs to flip for every term
 
-    for i in range(1, 50):
-        n = (i * 2) + 1
-        num = x * x
-        denom = n * (n - 1)
-        latest = older * (num / denom)
-        result += sign * latest
-        older = latest
-        sign *= -1
+    # keeping track of the previous term
+    num = x
+    denom = 1
+
+    for n in range(3, 50, 2):
+        num *= -x*x
+        denom *= n*(n-1)
+        result += num/denom
 
     return result
