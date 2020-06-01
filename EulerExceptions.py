f2ac0e59bf6979de15daea73e2620bc8c4b8425b
tarads443 <47217806+tarads443@users.noreply.github.com>
2020-05-31 22:40:15 -0400

    Delete EulerExceptions.py

--- EulerExceptions.py
+++
@@ -1,9 +0,0 @@
-# Base class for all custom exceptions
-class CalculatorException(Exception):
-    pass
-
-
-class InputError(CalculatorException):
-    def __init__(self, input_expression, message):
-        self.input_expression = input_expression
-        self.message = message
