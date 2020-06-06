13d2b7b9d1ff5d2b7a8cbd268f27e249a78c3c0a
neoJINXD <anik_patel@live.com>
2020-06-06 14:31:13 -0400

    Added temporary file for parser test

---
+++ test_parser.py
@@ -0,0 +1,44 @@
+import re
+from nparser import Parser
+import time
+
+
+def calc_eval(equation):
+    nsp = Parser()
+    try:
+        equation = re.sub('(?<=\d|\))(\()', '*(', equation)
+        equation = equation.replace('π', 'PI')
+        equation = equation.replace('e', 'E')
+        result = nsp.eval(equation)
+        return result
+    except:
+        return 'Unknown error occured.'
+
+
+if __name__ == '__main__':
+    start = time.time()
+    eq = 'sin(π)'
+    print(f'Calculating {eq}')
+    calc_eval(eq)
+    print(f'Took {time.time()-start}')
+    print()
+
+    start = time.time()
+    eq = '√(9)'
+    print(f'Calculating {eq}')
+    calc_eval(eq)
+    print(f'Took {time.time()-start}')
+    print()
+
+    start = time.time()
+    eq = 'e'
+    print(f'Calculating {eq}')
+    calc_eval(eq)
+    print(f'Took {time.time()-start}')
+    print()
+    
+    start = time.time()
+    eq = 'e^1'
+    print(f'Calculating {eq}')
+    calc_eval(eq)
+    print(f'Took {time.time()-start}')
