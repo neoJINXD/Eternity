7210e0a064da93516b1ec95fea1d1dc0805418f2
Emanuel <emanuel.sharma@unity3d.com>
2020-06-20 15:05:22 -0400

    Refactored code for greater consistency and PEP8 compliance
    Changed names
    Moved functions between files
    Added docstrings

--- functions/trignometry.py
+++ functions/trignometry.py
@@ -1,74 +1,151 @@
+import functions.common as common
 import functions.exponents_and_logs as exp
-import functions.exponent_helper_functions as exp_help
 
 
-def generate_pi() -> float:
-    """
-    Calculate pi using Nilakantha's infinite series which
-    converges faster than the Maclaurin series
+def generate_pi(term_count: int = 150) -> float:
+    """Returns an approximation of the value of PI.
+    
+    PI is approximated using Nilakantha's infinite series. 
+    
+    Args:
+        term_count (int): Number of terms of Nilakantha's series used to approximate PI
+ 
+    Returns:
+        float: Approximation of the value of PI
     """
     sign = 1
     pi = 3
-    for num in range(2, 15000, 2):
-        num1 = num + 1
-        num2 = num + 2
-        pi += (sign * 4 / (num * num1 * num2))
+    for i in range(2, term_count, 2):
+        pi += (sign * 4 / (i * (i + 1) * (i + 2)))
         sign *= -1
+        
     return pi
 
 
-def sin(x: float) -> float:
+def sin_taylor(angle: float, term_count: int = 150) -> float:
+    """Returns an approximation of the value of sin('angle') using the Maclaurin series expansion of sin(x).
+    
+    This approximation is typically used for values of 'angle' close to the center of the expansion (0).
+    Larger values of x require more terms to converge.
+    
+    Args:
+        angle (float): Input to sin function
+        term_count (int): Number of terms of Maclaurin series used to approximate value of sin('angle')
+ 
+    Returns:
+        float: Approximation of the value of sin('angle')
     """
-    Optimized version of sin using taylor expansion.
-    Based on Emanuel's suggestion to avoid factorial
-    and exponent calls.
-
-    Each term is it's previous value multiplied by
-    -(x*x)/(n(n-1)), where the starting point is x
-    """
-    # Initial values
-    result = x  # accumulator for the result
-
-    # keeping track of the previous term
-    numerator = x
+    result = angle
+    numerator = angle
     denominator = 1
-
-    for n in range(3, 50, 2):
-        numerator *= -x * x
-        denominator *= n * (n - 1)
+    for i in range(3, term_count, 2):
+        numerator *= -angle * angle
+        denominator *= i * (i - 1)
         result += numerator / denominator
 
     return result
 
 
-def cos(x: float) -> float:
+def cos_taylor(angle: float, term_count: int = 150) -> float:
+    """Returns an approximation of the value of cos('angle') using the Maclaurin series expansion of cos(x).
+    
+    This approximation is typically used for values of 'angle' close to the center of the expansion (0).
+    Larger values of x require more terms to converge.
+    
+    Args:
+        angle (float): Input to cos function
+        term_count (int): Number of terms of Maclaurin series used to approximate value of cos('angle')
+ 
+    Returns:
+        float: Approximation of the value of cos('angle')
     """
-    Following the same logic as sin
-    """
-    # Initial values
-    result = 1  # accumulator for the result
-
-    # keeping track of the previous term
-    num = 1
-    denom = 1
-
-    for n in range(2, 50, 2):
-        num *= -x * x
-        denom *= n * (n - 1)
-        result += num / denom
+    result = 1
+    numerator = 1
+    denominator = 1
+    for i in range(2, term_count, 2):
+        numerator *= -angle * angle
+        denominator *= i * (i - 1)
+        result += numerator / denominator
 
     return result
 
 
-def sinh(x: float) -> float:
-    """ Calculate hyperbolic function using value of e """
-    e = exp.generate_e(x)
-    return (e - exp_help.take_inverse(e)) / 2
+def sin(angle: float) -> float:
+    """Returns an approximation of the value of sin('angle').
+    
+    Args:
+        angle (float): Input to sin function
+ 
+    Returns:
+        float: Approximation of the value of sin('angle')
+    """
+    # Leverage cyclical property of trigonometric functions to keep angle close to center of Taylor series expansion.
+    return sin_taylor(angle % (2 * generate_pi()))
 
 
-def cosh(x: float) -> float:
-    return (exp.generate_e(x) + exp.generate_e(-x)) / 2
+def cos(angle: float) -> float:
+    """Returns an approximation of the value of cos('angle').
+    
+    Args:
+        angle (float): Input to cos function
+ 
+    Returns:
+        float: Approximation of the value of cos('angle')
+    """
+    # Leverage cyclical property of trigonometric functions to keep angle close to center of Taylor series expansion.
+    return cos_taylor(angle % (2 * generate_pi()))
 
 
-def tanh(x: float) -> float:
-    return sinh(x) / cosh(x)
+def tan(angle: float) -> float:
+    """Returns an approximation of the value of tan('angle').
+    
+    Args:
+        angle (float): Input to tan function
+ 
+    Returns:
+        float: Approximation of the value of tan('angle')
+    """
+    # Leverage cyclical property of trigonometric functions to keep angle close to center of Taylor series expansion.
+    angle %= 2 * generate_pi()
+    # Taylor series expansion of tan(x) is a little bit complicated. We will use tan(x)=sin(x)/cos(x) instead.
+    sin = sin_taylor(angle)
+    cos = cos_taylor(angle)
+    return sin/cos
+
+def sinh(argument: float) -> float:
+    """Returns an approximation of the value of sinh('argument').
+    
+    Args:
+        argument (float): Input to hyperbolic sin function
+ 
+    Returns:
+        float: Approximation of the value of sinh('argument')
+    """
+    e = exp.exp(argument)
+    return (e - common.inverse(e)) / 2
+
+
+def cosh(argument: float) -> float:
+    """Returns an approximation of the value of cosh('argument').
+    
+    Args:
+        argument (float): Input to hyperbolic cos function
+ 
+    Returns:
+        float: Approximation of the value of cosh('argument')
+    """
+    e = exp.exp(argument)
+    return (e + common.inverse(e)) / 2
+
+
+def tanh(argument: float) -> float:
+    """Returns an approximation of the value of tanh('argument').
+    
+    Args:
+        argument (float): Input to hyperbolic tan function
+ 
+    Returns:
+        float: Approximation of the value of tanh('argument')
+    """
+    e = exp.exp(argument)
+    return (e - common.inverse(e)) / (e + common.inverse(e))
--- tests/test_exponents_and_logs.py
+++ tests/test_exponents_and_logs.py
@@ -1,5 +1,5 @@
 import unittest
-import exceptions.exceptions as the_exception
+import exceptions.exceptions as exceptions
 import functions.exponents_and_logs as exp
 import functions.trignometry as trig
 import math as math
@@ -10,58 +10,58 @@ class ExponentsTest(unittest.TestCase):
 
     def test_e(self):
         # Tests euler number function's capacity for generating euler number.
-        self.assertAlmostEqual(exp.generate_e(), math.e, 15)
+        self.assertAlmostEqual(exp.exp(), math.e, 15)
 
-    def test_generate_e_input_0(self):
+    def test_exp_input_0(self):
         # Tests euler number function's capacity for handling input of 0.
-        self.assertAlmostEqual(exp.generate_e(0), math.pow(math.e, 0), 12)
+        self.assertAlmostEqual(exp.exp(0), math.pow(math.e, 0), 12)
 
-    def test_generate_e_input_neg(self):
+    def test_exp_input_neg(self):
         # Tests euler number function's capacity for handling negative inputs.
-        self.assertAlmostEqual(exp.generate_e(-10), math.pow(math.e, -10), 12)
-        self.assertAlmostEqual(exp.generate_e(-3), math.pow(math.e, -3), 12)
+        self.assertAlmostEqual(exp.exp(-10), math.pow(math.e, -10), 12)
+        self.assertAlmostEqual(exp.exp(-3), math.pow(math.e, -3), 12)
 
-    def test_generate_e_input_rational(self):
+    def test_exp_input_rational(self):
         # Tests euler number function's capacity for handling non-integer inputs.
-        self.assertAlmostEqual(exp.generate_e(0.5), math.pow(math.e, 0.5), 12)
-        self.assertAlmostEqual(exp.generate_e(5 / 8), math.pow(math.e, 5/8), 12)
+        self.assertAlmostEqual(exp.exp(0.5), math.pow(math.e, 0.5), 12)
+        self.assertAlmostEqual(exp.exp(5 / 8), math.pow(math.e, 5/8), 12)
 
-    def test_generate_e_input_big(self):
+    def test_exp_input_big(self):
         # Tests euler number function's capacity for handling big numbers
-        self.assertAlmostEqual(exp.generate_e(32), pow(math.e, 32), 0)  # Accuracy needs work for big numbers, decimal places incorrect
-        self.assertAlmostEqual(exp.generate_e(18), pow(math.e, 18), 6)
+        self.assertAlmostEqual(exp.exp(32), pow(math.e, 32))  # Accuracy needs work for big numbers, decimal places incorrect
+        self.assertAlmostEqual(exp.exp(18), pow(math.e, 18))
 
-    def test_generate_e_input_transcendental(self):
+    def test_exp_input_transcendental(self):
         # Tests euler number function's capacity for handling other transcendental numbers such as pi as input
-        self.assertAlmostEqual(exp.generate_e(trig.generate_pi()), pow(math.e, math.pi))
-        self.assertAlmostEqual(exp.generate_e(exp.generate_e()), pow(math.e, math.e))
+        self.assertAlmostEqual(exp.exp(trig.generate_pi()), pow(math.e, math.pi))
+        self.assertAlmostEqual(exp.exp(exp.exp()), pow(math.e, math.e))
 
     def test_exponent_input_pos(self):
         # Tests exponent function's capacity for handling positive integer inputs.
-        self.assertAlmostEqual(exp.calculate_exponent(2, 3), 2 ** 3)
+        self.assertAlmostEqual(exp.pow(2, 3), 2 ** 3)
 
     def test_exponent_input_neg(self):
         # Tests exponent function's capacity for handling negative integer inputs.
-        self.assertAlmostEqual(exp.calculate_exponent(200, -99), 200 ** -99)
-        self.assertAlmostEqual(exp.calculate_exponent(997, -15), 997 ** -15)
-        self.assertAlmostEqual(exp.calculate_exponent(8000, -150), 8000 ** -150)
+        self.assertAlmostEqual(exp.pow(200, -99), 200 ** -99)
+        self.assertAlmostEqual(exp.pow(997, -15), 997 ** -15)
+        self.assertAlmostEqual(exp.pow(8000, -150), 8000 ** -150)
 
     def test_zero_power_zero(self):
         # Tests 0 to the power of 0
-        self.assertAlmostEqual(exp.calculate_exponent(0, 0), 0 ** 0)
+        self.assertAlmostEqual(exp.pow(0, 0), 0 ** 0)
 
     def test_exponent_input_rational(self):
         # Tests exponent function's capacity for handling non-integer inputs.
-        self.assertAlmostEqual(exp.calculate_exponent(3, (1 / 3)), 3 ** (1 / 3), 4)
-        self.assertAlmostEqual(exp.calculate_exponent(100, (1 / 20)), 100 ** (1 / 20), 4)
-        self.assertAlmostEqual(exp.calculate_exponent(200, (1 / 40)), 200 ** (1 / 40), 4)
-        self.assertAlmostEqual(exp.calculate_exponent((63 / 40), (1 / 40)), (63 / 40) ** (1 / 40),4)
+        self.assertAlmostEqual(exp.pow(3, (1 / 3)), 3 ** (1 / 3))
+        self.assertAlmostEqual(exp.pow(100, (1 / 20)), 100 ** (1 / 20))
+        self.assertAlmostEqual(exp.pow(200, (1 / 40)), 200 ** (1 / 40))
+        self.assertAlmostEqual(exp.pow((63 / 40), (1 / 40)), (63 / 40) ** (1 / 40))
 
     def test_rational_power_rational(self):
         # Tests a rational number to the power of a rational number
-        self.assertAlmostEqual(exp.calculate_exponent((1 / 3), (1 / 3)), (1 / 3) ** (1 / 3), 5)
-        self.assertAlmostEqual(exp.calculate_exponent((1 / 20), (2 / 9)), (1 / 20) ** (2 / 9), 5)
-        self.assertAlmostEqual(exp.calculate_exponent((2 ** (1 / 2)), (2 ** (1 / 2))), math.sqrt(2) ** math.sqrt(2), 5)
+        self.assertAlmostEqual(exp.pow((1 / 3), (1 / 3)), (1 / 3) ** (1 / 3), 5)
+        self.assertAlmostEqual(exp.pow((1 / 20), (2 / 9)), (1 / 20) ** (2 / 9), 5)
+        self.assertAlmostEqual(exp.pow((2 ** (1 / 2)), (2 ** (1 / 2))), math.sqrt(2) ** math.sqrt(2), 5)
 
     def test_logarithm_input_int(self):
         # Tests logarithm function's capacity for handling positive integer inputs.
@@ -81,41 +81,35 @@ class ExponentsTest(unittest.TestCase):
     def test_logarithm_exceptions(self):
         # Tests exponent function's exception handling capacity.
         # Test handling of invalid base
-        with self.assertRaises(ValueError):
+        with self.assertRaises(exceptions.InputError):
             exp.log(20, 0)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(exceptions.InputError):
             exp.log(20, 1)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(exceptions.InputError):
             exp.log(20, -1)
-        with self.assertRaises(TypeError):
-            exp.log(20, "hello")
         # Test handling of invalid argument
-        with self.assertRaises(ValueError):
+        with self.assertRaises(exceptions.InputError):
             exp.log(-5, 2)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(exceptions.InputError):
             exp.log(0, 2)
-        with self.assertRaises(TypeError):
-            exp.log(complex(2, 1), 2)
-        with self.assertRaises(TypeError):
-            exp.log(None, 2)
 
-    def test_power_of_ten(self):
+    def test_pow_10(self):
         # Tests power of ten function.
-        self.assertAlmostEqual(exp.power_of_ten(0), 1)
-        self.assertAlmostEqual(exp.power_of_ten(1), 10)
-        self.assertAlmostEqual(exp.power_of_ten(2), 100)
-        self.assertAlmostEqual(exp.power_of_ten(-4), 10 ** (-4))
-        self.assertAlmostEqual(exp.power_of_ten(0.01), 10 ** 0.01)
-        self.assertAlmostEqual(exp.power_of_ten(self.PI), 10 ** self.PI, 1)
+        self.assertAlmostEqual(exp.pow_10(0), 1)
+        self.assertAlmostEqual(exp.pow_10(1), 10)
+        self.assertAlmostEqual(exp.pow_10(2), 100)
+        self.assertAlmostEqual(exp.pow_10(-4), 10 ** (-4))
+        self.assertAlmostEqual(exp.pow_10(0.01), 10 ** 0.01)
+        self.assertAlmostEqual(exp.pow_10(self.PI), 10 ** self.PI)
 
-    def test_power_of_pi(self):
+    def test_pow_pi(self):
         # Tests power of pi function.
-        self.assertAlmostEqual(exp.power_of_pi(0), 1)
-        self.assertAlmostEqual(exp.power_of_pi(1), self.PI)
-        self.assertAlmostEqual(exp.power_of_pi(2), self.PI ** 2)
-        self.assertAlmostEqual(exp.power_of_pi(-4), self.PI ** (-4))
-        self.assertAlmostEqual(exp.power_of_pi(0.01), self.PI ** 0.01)
-        self.assertAlmostEqual(exp.power_of_pi(self.PI), self.PI ** self.PI, 3)
+        self.assertAlmostEqual(exp.pow_pi(0), 1)
+        self.assertAlmostEqual(exp.pow_pi(1), self.PI)
+        self.assertAlmostEqual(exp.pow_pi(2), self.PI ** 2)
+        self.assertAlmostEqual(exp.pow_pi(-4), self.PI ** (-4))
+        self.assertAlmostEqual(exp.pow_pi(0.01), self.PI ** 0.01)
+        self.assertAlmostEqual(exp.pow_pi(self.PI), self.PI ** self.PI)
 
 
 if __name__ == '__main__':
