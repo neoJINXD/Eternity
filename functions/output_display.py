a3ea14e124ff483b04d7872b32c0df8736aacfdd
SwatiPareek <38918266+SwatiPareek@users.noreply.github.com>
2020-06-30 21:03:23 -0400

    Added degree to radian and radian to degree function

---
+++ functions/output_display.py
@@ -0,0 +1,27 @@
+import functions.trignometry as trig
+
+
+def rad(deg: float) -> float:
+    """Returns the radians corresponding to input angle
+
+       Args:
+           deg (float): Angle in degree
+
+       Returns:
+           float: Radian of input
+    """
+
+    return deg * trig.generate_pi() / 180
+
+
+def deg(rad: float) -> float:
+    """Returns the degree corresponding to input angle
+
+       Args:
+          rad (float): Angle in radians
+
+       Returns:
+           float: Degree of input
+    """
+
+    return rad * 180 / trig.generate_pi()
