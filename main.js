8ffd397d520daf808bbaca46984e1efa2c9a2fa1
neoJINXD <neojinxd@gmail.com>
2020-07-08 20:50:56 -0400

    Fixed javascript linting errors

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-const {app, BrowserWindow} = require('electron');
-const path = require('path');
-const url = require('url');
-const pyshell =  require('python-shell');
+/* eslint-disable-next-line */
+const { app, BrowserWindow } = require('electron');
 
 // Connecting with our web app to use the same API
-const express = require('./index'); 
+require('./index');
 
 // Initializing the electron app
 
-function createWindow () {
-    //express();
-    let window = new BrowserWindow({
-      width: 800, 
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true
-      }
-    })
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    window.loadURL('http://localhost:3000/');
+  window.loadURL('http://localhost:3000/');
 }
 
-
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    //if (process.platform !== 'darwin') {
-    app.quit()
-    //}
-})
-
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
--- public/serverScripts.js
+++ public/serverScripts.js
@@ -1,46 +1,54 @@
+// Eslint check disabled here since the functions are used within the index.html file
+
 // function to add character based on what button was pressed
-function addToExpression(i){
-    document.getElementById('resultInput').value = document.getElementById('resultInput').value + i;
+/* eslint-disable-next-line */
+function addToExpression(i) {
+  document.getElementById('resultInput').value = document.getElementById('resultInput').value + i;
 }
 
 // function to clear the input field
-function clearInput() { 
-    document.getElementById('resultInput').value = '' 
+/* eslint-disable-next-line */
+function clearInput() {
+  document.getElementById('resultInput').value = '';
 }
 
 // function to delete the last character inputted
-function del() { 
-    str = document.getElementById('resultInput').value;
-    document.getElementById('resultInput').value = str.substring(0, str.length - 1)
+/* eslint-disable-next-line */
+function del() {
+  const str = document.getElementById('resultInput').value;
+  document.getElementById('resultInput').value = str.substring(0, str.length - 1);
 }
 
 // function to delete the datalist
+/* eslint-disable-next-line */
 function clr() {
-    document.getElementById('browserList').innerHTML = '';
+  document.getElementById('browserList').innerHTML = '';
 }
 
-// function to send a request to node to run the python command for calculating the result of the expression
+// function to send a request to node to run the python command
+// for calculating the result of the expression
+/* eslint-disable-next-line */
 async function calc() {
-    // gets the input from teh textbox
-    const input = { expression: document.getElementById('resultInput').value };
-    document.getElementById('equation').innerHTML = `> ${input.expression}`;
+  // gets the input from teh textbox
+  const input = { expression: document.getElementById('resultInput').value };
+  document.getElementById('equation').innerHTML = `> ${input.expression}`;
 
-    dlist = document.getElementById('browserList');
-    option = document.createElement('option');
-    option.value = document.getElementById('resultInput').value;
-    dlist.appendChild(option);
+  const dlist = document.getElementById('browserList');
+  const option = document.createElement('option');
+  option.value = document.getElementById('resultInput').value;
+  dlist.appendChild(option);
 
-    // calls node to perform the calculation over the route
-    const response = await fetch(`http://localhost:3000/math`, {
-        method: 'POST',
-        body: JSON.stringify(input),
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
-    const json = await response.json();
+  // calls node to perform the calculation over the route
+  const response = await fetch('http://localhost:3000/math', {
+    method: 'POST',
+    body: JSON.stringify(input),
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  const json = await response.json();
 
-    // Sets the answer in the input text
-    document.getElementById('answer').innerHTML = `= ${json.answer}`;
-    document.getElementById('resultInput').value = '';
+  // Sets the answer in the input text
+  document.getElementById('answer').innerHTML = `= ${json.answer}`;
+  document.getElementById('resultInput').value = '';
 }
