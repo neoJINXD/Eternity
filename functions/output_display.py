d3734ac46d2b7026cfa2004449e58f58d33d8729
SwatiPareek <38918266+SwatiPareek@users.noreply.github.com>
2020-07-15 17:28:56 -0400

    Added initial deg functions

---
+++ functions/output_display.py
@@ -0,0 +1,23 @@
+import functions.trigonometry as trig
+
+
+def rad(deg: float) -> float:
+    """Returns the radians corresponding to input angle
+       Args:
+           deg (float): Angle in degree
+       Returns:
+           float: Radian of input
+    """
+
+    return deg * trig.generate_pi() / 180
+
+
+def deg(rad: float) -> float:
+    """Returns the degree corresponding to input angle
+       Args:
+          rad (float): Angle in radians
+       Returns:
+           float: Degree of input
+    """
+
+    return rad * 180 / trig.generate_pi()
---
+++ tests/test_output_display.py
@@ -0,0 +1,27 @@
+import unittest
+import functions.output_display as display
+import math
+
+
+class TestOutputDisplayFunctions(unittest.TestCase):
+    '''
+    Unit tests for deg, radian, scientific notation, binary functions
+    '''
+
+    def test_deg(self):
+        self.assertAlmostEqual(display.deg(1), math.degrees(1), 2)
+        self.assertAlmostEqual(display.deg(0), math.degrees(0), 2)
+        self.assertAlmostEqual(display.deg(180), math.degrees(180), 2)
+        self.assertAlmostEqual(display.deg(3953), math.degrees(3953), 2)
+        self.assertAlmostEqual(display.deg(290.57), math.degrees(290.57), 2)
+        self.assertAlmostEqual(display.deg(math.pi), math.degrees(math.pi), 2)
+        self.assertAlmostEqual(display.deg(-11), math.degrees(-11), 2)
+
+    def test_rad(self):
+        self.assertAlmostEqual(display.rad(1), math.radians(1), 2)
+        self.assertAlmostEqual(display.rad(0), math.radians(0), 2)
+        self.assertAlmostEqual(display.rad(180), math.radians(180), 2)
+        self.assertAlmostEqual(display.rad(39532), math.radians(39532), 2)
+        self.assertAlmostEqual(display.rad(290.57), math.radians(290.57), 2)
+        self.assertAlmostEqual(display.rad(math.pi), math.radians(math.pi), 2)
+        self.assertAlmostEqual(display.rad(-11), math.radians(-11), 2)
