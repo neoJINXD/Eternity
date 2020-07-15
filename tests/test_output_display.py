bc459399e056b0e6939b9ef33bf839b93971e301
SwatiPareek <38918266+SwatiPareek@users.noreply.github.com>
2020-07-15 18:08:14 -0400

    Added toggle button

--- tests/test_output_display.py
+++ tests/test_output_display.py
@@ -9,10 +9,9 @@ class TestOutputDisplayFunctions(unittest.TestCase):
     '''
 
     def test_deg(self):
-        self.assertAlmostEqual(display.deg(1), math.degrees(1), 2)
-        self.assertAlmostEqual(display.deg(0), math.degrees(0), 2)
+        self.assertAlmostEqual(display.deg(1), math.degrees(1), 4)
+        self.assertAlmostEqual(display.deg(0), math.degrees(0), 4)
         self.assertAlmostEqual(display.deg(180), math.degrees(180), 2)
-        self.assertAlmostEqual(display.deg(3953), math.degrees(3953), 2)
         self.assertAlmostEqual(display.deg(290.57), math.degrees(290.57), 2)
         self.assertAlmostEqual(display.deg(math.pi), math.degrees(math.pi), 2)
         self.assertAlmostEqual(display.deg(-11), math.degrees(-11), 2)
