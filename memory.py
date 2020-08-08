b4a3abb46f808f2826d2982d7d757dd89208b121
cpotteck <cpotteck@gmail.com>
2020-08-08 01:24:31 -0400

    fix memory test errors

--- memory.py
+++ memory.py
@@ -2,7 +2,8 @@ import sys
 import re
 from parse import Parser
 
-expression = sys.argv[1]
+expression = sys.argv[1] if len(sys.argv) > 1 else ""
+
 history = []
 
 
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,15 +1,10 @@
 import unittest
-# import memory
+import memory
 import math
 import exceptions.exceptions as the_exception
 
-# test class
-# class TestMemory(unittest.TestCase):
-#     pass
-#     # def test_factorial(self):
-#     #     self.assertEqual(common.factorial(6), math.factorial(6))
-#     #     self.assertEqual(common.factorial(0), math.factorial(0))  # Test input of 0
-#     #     self.assertEqual(common.factorial(345), math.factorial(345))  # Test large number
+class TestMemoryFunctions(unittest.TestCase):
+    pass
 
 # Allows us to run the tests immediately when this file is run
 if __name__ == '__main__':
